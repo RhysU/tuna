068aca7d8acd01425c6fe4002fcce88b7672f173
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-10-24 00:27:38 -0500

    Drafting basic logic

--- examples/basic.c
+++ examples/basic.c
@@ -6,13 +6,46 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include <tuna/tuna.h>
+#include "tuna/stats.h"
 
+const double u01(void) { return rand() / (double) RAND_MAX; }
+
+// This is currently a work-in-progress to discover the correct APIs.
 int main(int argc, char *argv[])
 {
-    /* TODO */
+    srand(time(NULL));
+
+    // Parse and display any incoming command line arguments in a header
+    const int    niter = argc > 1 ? atoi(argv[1]) : 10; // How many iterations?
+    const double m0    = argc > 2 ? atof(argv[2]) : 10; // Case 1 mean elapsed?
+    const double r0    = argc > 3 ? atof(argv[3]) : 1;  // Case 1 stddev elapsed?
+    const double m1    = argc > 4 ? atof(argv[4]) : 10; // Case 1 mean elapsed?
+    const double r1    = argc > 5 ? atof(argv[5]) : 1;  // Case 1 stddev elapsed?
+    const int    b     = argc > 6 ? atof(argv[6]) : 5;  // How much burn in?
+    printf("# niter=%d, m0=%g, r0=%g, m1=%g, r1=%g\n", niter, m0, r0, m1, r1);
+
+    tuna_stats s[2];
+    for (int i = 0; i < niter; ++i) {
+        // Which branch should be taken this iteration?
+        const int ndx = tuna_stats_cnt(s+0) < b ? 0
+                      : tuna_stats_cnt(s+1) < b ? 1
+                      : tuna_stats_welch1(s+0,s+1) < u01();
+
+        // Take the branch, tracking the "elapsed" time.
+        double elapsed;
+        switch (ndx) {
+            case 0: elapsed = m0 + r0*(u01()-0.5); break;
+            case 1: elapsed = m1 + r1*(u01()-0.5); break;
+        }
+        tuna_stats_obs(s+ndx, elapsed);
+
+        // Output the chosen branch and behavior for debugging purposes
+        printf("%6d\t%d\t%g\n", i, ndx, elapsed);
+    }
+
     return EXIT_SUCCESS;
 }
