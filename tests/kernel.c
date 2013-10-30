70dae2f0910144b179af509028c52b549a9df100
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-10-29 23:38:15 -0500

    Stub tests for tuna_kernel

---
+++ tests/kernel.c
@@ -0,0 +1,24 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2013 Rhys Ulerich
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+#include <tuna/kernel.h>
+
+#include "fct.h"
+
+FCT_BGN()
+{
+    FCT_QTEST_BGN(examine)
+    {
+        // TODO
+    }
+    FCT_QTEST_END();
+
+}
+FCT_END()
