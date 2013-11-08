31ace98d3ba3bf379561457448082acbe2c0e0b7
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-11-08 10:35:55 -0600

    Cut down study space

--- study.sh
+++ study.sh
@@ -5,5 +5,5 @@ which parallel >/dev/null 2>&1 || (echo 'GNU parallel needed (http://www.gnu.org
 which stats    >/dev/null 2>&1 || (echo 'Program stats needed (https://github.com/rustyrussell/stats)' && false)
 parallel --gnu --noswap --nice=10 --eta -j+0                                               \
          '(for i in $(seq 1 {1}); do ./examples/basic {2}; done) | stats > result.{1}.{2}' \
-                  ::: 1e4 5e3 2e3 1e3 5e2 2e2 1e2 5e1 2e1 1e1                              \
+                  ::: 1e4                                                                  \
                   ::: 1e4 5e3 2e3 1e3 5e2 2e2 1e2 5e1 2e1 1e1
