0045e947cccfffdc876e839cf81bf82aa0d6155f
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-11-08 09:42:43 -0600

    Simplify study output

--- study.sh
+++ study.sh
@@ -3,9 +3,7 @@
 set -eu
 which parallel >/dev/null 2>&1 || (echo 'GNU parallel needed (http://www.gnu.org/software/parallel/)'  && false)
 which stats    >/dev/null 2>&1 || (echo 'Program stats needed (https://github.com/rustyrussell/stats)' && false)
-parallel --gnu --noswap --nice=10 --eta -j+0                                             \
-         --header :                                                                      \
-         --results study                                                                 \
-         '(for i in $(seq 1 {trials}); do ./examples/basic {invocations}; done) | stats' \
-         ::: trials      10 20 50 100 200 500 1000 2000 5000 10000                       \
-         ::: invocations 10 20 50 100 200 500 1000 2000 5000 10000
+parallel --gnu --noswap --nice=10 --eta -j+0                                               \
+         '(for i in $(seq 1 {1}); do ./examples/basic {2}; done) | stats > result.{1}.{2}' \
+                  ::: 1e4 5e3 2e3 1e3 5e2 2e2 1e2 5e1 2e1 1e1                              \
+                  ::: 1e4 5e3 2e3 1e3 5e2 2e2 1e2 5e1 2e1 1e1
