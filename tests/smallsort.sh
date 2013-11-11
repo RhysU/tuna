a7e8a3f8e214d7768e4aa810a54c20ecd6d52bb8
Rhys Ulerich <rhys.ulerich@gmail.com>
2013-11-10 23:42:30 -0600

    Tweak run params

--- tests/smallsort.sh
+++ tests/smallsort.sh
@@ -2,13 +2,13 @@
 set -eu
 
 echo Default algorithm
-export TUNA_ALGO=;             ./examples/smallsort
+export TUNA_ALGO=;             ./examples/smallsort 1000 160
 echo ""
 
 echo Welch1 algorithm
-export TUNA_ALGO=welch1;       ./examples/smallsort
+export TUNA_ALGO=welch1;       ./examples/smallsort 1000 160
 echo ""
 
 echo Welch1 algorithm, \\nu_ \\to \\infty limit.
-export TUNA_ALGO=welch1_nuinf; ./examples/smallsort
+export TUNA_ALGO=welch1_nuinf; ./examples/smallsort 1000 160
 echo ""
