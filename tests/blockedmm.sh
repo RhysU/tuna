634f100847e2419d596e50cade593abb19eea30e
Rhys Ulerich <rhys.ulerich@gmail.com>
2020-01-05 15:39:33 -0500

    Speed unit test + add zero case

--- tests/blockedmm.sh
+++ tests/blockedmm.sh
@@ -2,13 +2,17 @@
 set -eu
 
 echo Default algorithm
-export TUNA_ALGO=;             OMP_NUM_THREADS=1 ./examples/blockedmm 128 7
+export TUNA_ALGO=;             OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
+echo ""
+
+echo Degenerate algorithm
+export TUNA_ALGO=zero;         OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
 echo ""
 
 echo Welch1 algorithm
-export TUNA_ALGO=welch1;       OMP_NUM_THREADS=2 ./examples/blockedmm 128 7
+export TUNA_ALGO=welch1;       OMP_NUM_THREADS=2 ./examples/blockedmm 64 7
 echo ""
 
 echo Welch1 algorithm, \\nu_ \\to \\infty limit.
-export TUNA_ALGO=welch1_nuinf; OMP_NUM_THREADS=2 ./examples/blockedmm 128 7
+export TUNA_ALGO=welch1_nuinf; OMP_NUM_THREADS=2 ./examples/blockedmm 64 7
 echo ""
