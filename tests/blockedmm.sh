9f445e29717a2d0ca90dddda8eac91964e2a75ac
Rhys Ulerich <rhys.ulerich@gmail.com>
2026-01-20 22:37:39 -0500

    Revise comments slightly

--- tests/blockedmm.sh
+++ tests/blockedmm.sh
@@ -9,6 +9,10 @@ echo Degenerate algorithm
 export TUNA_ALGO=zero;         OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
 echo ""
 
+echo Uniform algorithm
+export TUNA_ALGO=uniform;      OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
+echo ""
+
 echo Welch1 algorithm
 export TUNA_ALGO=welch1;       OMP_NUM_THREADS=2 ./examples/blockedmm 64 7
 echo ""
