904d890925104fdb8eb1af81f930dcb6c76871e4
Rhys Ulerich <rhys.ulerich@gmail.com>
2026-01-20 22:42:15 -0500

    Better label outputs

--- tests/blockedmm.sh
+++ tests/blockedmm.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -eu
 
-echo Default algorithm
-export TUNA_ALGO=;             OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
+echo Default algorithm with 1 thread
+export TUNA_ALGO=;             OMP_NUM_THREADS=1 ./examples/blockedmm 128 7
 echo ""
 
-echo Degenerate algorithm
-export TUNA_ALGO=zero;         OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
+echo Degenerate algorithm with 1 thread
+export TUNA_ALGO=zero;         OMP_NUM_THREADS=1 ./examples/blockedmm 128 7
 echo ""
 
-echo Uniform algorithm
-export TUNA_ALGO=uniform;      OMP_NUM_THREADS=1 ./examples/blockedmm 64 7
+echo Uniform algorithm with 1 thread
+export TUNA_ALGO=uniform;      OMP_NUM_THREADS=1 ./examples/blockedmm 128 7
 echo ""
 
-echo Welch1 algorithm
-export TUNA_ALGO=welch1;       OMP_NUM_THREADS=2 ./examples/blockedmm 64 7
+echo Welch1 algorithm with 2 threads
+export TUNA_ALGO=welch1;       OMP_NUM_THREADS=2 ./examples/blockedmm 128 7
 echo ""
 
-echo Welch1 algorithm, \\nu_ \\to \\infty limit.
-export TUNA_ALGO=welch1_nuinf; OMP_NUM_THREADS=2 ./examples/blockedmm 64 7
+echo Welch1 algorithm, \\nu_ \\to \\infty limit, with 2 threads
+export TUNA_ALGO=welch1_nuinf; OMP_NUM_THREADS=2 ./examples/blockedmm 128 7
 echo ""
